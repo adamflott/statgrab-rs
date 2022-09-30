6a9401850e8c417aa4fb63f21865f56db47c9f36
Adam Flott <adam@adamflott.com>
2022-09-30 08:48:52 -0400

    Fix nix-shell package name

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   #pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/048958000f08aebb9ff0350a5b09d8f8a17c674e.tar.gz")) {};
   pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
 in pkgs.clangStdenv.mkDerivation {
-  name = "libstatgrab-sys";
+  name = "statgrab";
   buildInputs = [ pkgs.cargo pkgs.rustc pkgs.libstatgrab pkgs.libclang ];
   LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang.lib}/lib";
 }
